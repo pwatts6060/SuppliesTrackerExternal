18fb4b45b2e18f160de6133d20a97768c18c8ec7
mthimm2 <70667658+mthimm2@users.noreply.github.com>
2024-01-06 22:10:45 -0800

    Update SuppliesTrackerPlugin.java
    Another typo

--- src/main/java/com/suppliestracker/SuppliesTrackerPlugin.java
+++ src/main/java/com/suppliestracker/SuppliesTrackerPlugin.java
@@ -1537,7 +1537,7 @@ public class SuppliesTrackerPlugin extends Plugin
 						itemManager.getItemPrice(FIRE_RUNE) * 5L + itemManager.getItemPrice(COINS_995) * 10L;
 				break;
 			case WARPED_SCEPTRE:
-				calculated_price = itemManager.getItemPrice(CHAOS_RUNE) * 2L + itemManager.getItemPrice(EARTH_RUNE) * 5L;
+				calculatedPrice = itemManager.getItemPrice(CHAOS_RUNE) * 2L + itemManager.getItemPrice(EARTH_RUNE) * 5L;
 				break;
 			case SANGUINESTI_STAFF:
 				calculatedPrice = itemManager.getItemPrice(BLOOD_RUNE) * 3L;
